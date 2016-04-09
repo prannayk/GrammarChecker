70ce39ca2b3ebb18e731a3afc2dab4101eb2753c
Prannay Khosla <prannay.khosla@gmail.com>
2016-04-10 04:33:07 +0530

    Completed first implementation, next step follow implementation, followed by empty to check for conflicts

--- Layout.hs
+++ Layout.hs
@@ -8,21 +8,26 @@ type Aring = [Atom]
 data Production = Produce Aring [Aring] 
 type Grammar = ([Terminal],[NonTerminal],[Production])
 
-first :: Aring -> [Aring]
-first ((Term x):_) = [[Term x]]
-first ((NonTerm x):_) = (match [(NonTerm x)]) >>= first
-match ::  Aring -> [Aring]
-match (NonTerm "A") = [[(Term "a")],[( Term "b")]]
+first :: Grammar  -> Aring -> [Aring]
+first _ ((Term x):_)= [[Term x]]
+first grmmr ((NonTerm x):_) = (match [(NonTerm x)] grmmr) >>= first grmmr
 searchTinAtom :: Aring -> Terminal -> [Bool]
 searchTinAtom ring x = [(Term x) `elem` ring]
 searchTinAring1 ::  [Terminal] -> Production -> [Bool]
 searchTinAring1 terms (Produce head _) = [and $ terms >>= searchTinAtom head]
 contextFree :: Grammar -> Bool
 contextFree (terms,nterms,prods) = and $ prods >>= searchTinAring1 terms
-leftRecur :: Production -> [Bool]
-leftRecur (Produce head tails) = [and $ tails >>= first >>= checkFirst head]
+leftRecur :: Grammar -> Production -> [Bool]
+leftRecur grmmr (Produce head tails) = [and $ tails >>= first grmmr >>= checkFirst head]
 checkFirst :: Aring -> Aring -> [Bool]
 checkFirst head target = [(head == (take (length head) target))]
 leftRecurGr :: Grammar -> Bool
-leftRecurGr (terms,nterms,prods) = and $ prods >>= leftRecur
+leftRecurGr (terms,nterms,prods) = and $ prods >>= leftRecur (terms,nterms,prods)
 
+checkLeft :: Aring -> Production -> [Aring]
+checkLeft ring (Produce ringo rings) 
+	| ring == ringo = rings
+
+match ::  Aring -> Grammar -> [Aring]
+match [(NonTerm "A")] _ = [[(Term "a")],[( Term "b")]]
+match ring (_,_,prods) = prods >>= checkLeft ring
