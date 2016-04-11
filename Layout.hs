27416743f0be6f992b1292f033df9f37ab9d4954
Prannay Khosla <prannay.khosla@gmail.com>
2016-04-12 03:36:14 +0530

    Added functionality

--- Layout.hs
+++ Layout.hs
@@ -32,6 +32,23 @@ checkFirst head target = [(head == (take (length head) target))]
 leftRecurGr :: Grammar -> Bool
 leftRecurGr (terms,nterms,prods) = and $ prods >>= leftRecur (terms,nterms,prods)
 -- the above checks for left recursive grammar as a whole and if the grammar is recursive, we will have to remove that recursion
+rmLeftRecursion :: [Production] -> [Production]
+rmLeftRecursion [] = []
+rmLeftRecursion (prod:prods) = rmLeft prod (rmLeftRecursion prods)
+rmLeft :: Production -> [Production] -> [Production]
+rmLeft (Produce ring rings) dones = ((filter (isNterm dones) rings) >>= getAlternate dones ring) ++ ((filter (hasTerm) rings) >>= (\x -> [(Produce ring [x])]))
+getAlternate :: [Production] -> Aring -> Aring -> [Production]
+getAlternate dones (ringo:rango) head = [Produce head (map (++rango) (match [ringo] ([],[],dones)))]
+isNterm :: [Production] -> Aring -> Bool
+isNterm prods (atom:other) = if (nonTerm atom) then and $ prods >>= (\x -> if (prodHas x atom) then [True] else [False]) else False
+hasTerm :: Aring -> Bool
+hasTerm ((Term _):_) = True
+hasTerm _ = False
+nonTerm :: Atom -> Bool
+nonTerm (NonTerm _) = True
+nonTerm _ = False
+prodHas :: Production -> Atom -> Bool
+prodHas (Produce (atom:ring) tails) atom2 = atom == atom2
 checkLeft :: Aring -> Production -> [Aring]
 checkLeft ring (Produce ringo rings) 
 	| ring == ringo = rings
@@ -44,3 +61,13 @@ countF grmmer term nonterm = foldl (\acc x -> if(x==True) then acc+1 else acc) 0
 --check for ll1
 ll1 :: Grammar -> Bool
 ll1 (terms,nterms,prods) = and $ map (<1) $ countF (terms,nterms,prods) <$> terms <*> nterms
+
+-- left factor the grammar
+factor :: [[a]] -> [a]
+factor x:[] = []
+factor (x:xs):xss = longer (x ++ factor (filter (\(y:ys) -> x == y) xss)) (factor xss)
+
+longer :: [a] -> [a] -> [a]
+longer list1 list2
+	| (length list1) > (length list2) = list1
+	| otherwise = list2
